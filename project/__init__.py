13cbb05325a99cff3a208c9dc59e19f2242887c7
Ananya Hans <ahbwc@mail.missouri.edu>
2021-09-28 16:36:02 -0500

    routes being hit

--- project/__init__.py
+++ project/__init__.py
@@ -14,4 +14,13 @@ def create_app():
     db.init_app(app)
 
     #blueprint for auth routes in our app
-    from ../auth import auth as auth_blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    #blueprint for non-auth partf of the applicaiton
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+    
